4c40bdea03b0e87eb79e26df01f7ea85b008697d
Geraud Magne <geraud.magne@epita.fr>
2019-07-04 19:16:49 +0200

    [GAME] added constructors and play_word function

--- includes/game.hh
+++ includes/game.hh
@@ -1,1 +1,21 @@
 #pragma once
+
+#include <array>
+#include <utility>
+#include "grid.hh"
+#include "position.hh"
+
+class Game
+{
+private:
+    std::array<std::string, 4> players_; // players_[0] and players_[1] are together
+    grid::Grid grid_;
+    int player_;
+
+public:
+    Game(std::string p1, std::string p2, std::string p3, std::string p4, int player);
+    Game(int player);
+
+    int play_word(grid::Position p, Color c);
+
+};
--- src/game.cpp
+++ src/game.cpp
@@ -1,1 +1,22 @@
 #include "game.hh"
+
+Game::Game(std::string p1, std::string p2, std::string p3, std::string p4, int player)
+{
+    assert(player >= 1 && player <= 4);
+    players_ = {std::move(p1), std::move(p2), std::move(p3), std::move(p4)};
+    grid_ = grid::Grid();
+    this->player_ = player;
+}
+
+Game::Game(int player) : Game("player1", "player2", "player3", "player4", player)
+{}
+
+int Game::play_word(grid::Position p, Color c) // -1: error, 0: all good / 1: stop / 2: dead
+{
+    Word w = grid_.get(p.x, p.y);
+    if (w.isFlipped())
+        return -1;
+    if (c == w.getColor())
+        return
+}
+
