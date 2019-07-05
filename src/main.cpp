f1094f5978bb91693ee62a2db23fef6ddca65e03
Geraud Magne <geraud.magne@epita.fr>
2019-07-05 02:04:35 +0200

    [MAIN] better main, divided functions

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,9 @@
 #include <game.hh>
 
 
-int main()
+int get_player_n()
 {
+input:
     std::string temp;
     std::cout << "Quel est ton numéro de joueur?" <<
               " (1 et 2, bleus, jouent contre 3 et 4, rouge)" <<
@@ -11,21 +12,29 @@ int main()
 
     getline(std::cin, temp);
 
-    int plr = -1;
-    plr = atoi(temp.c_str());
 
-    if (plr != 1 && plr != 2 && plr != 3 && plr != 4)
+    int ret = -1;
+    try
     {
-        std::cout << "On te demande un chiffre entre 1 et 4 omg";
-        return 1;
+        ret = std::stoi(temp);
+    }
+    catch (std::exception& e)
+    {
+        std::cout << "bon je vais répeter. ";
+        goto input;
     }
 
-    Game game = Game(plr);
+    if (ret != 1 && ret != 2 && ret != 3 && ret != 4)
+    {
+        std::cout << "On te demande un chiffre entre 1 et 4 omg";
+        goto input;
+    }
 
-    int winner = game.loop(plr);
-
+    return ret;
+}
+void print_winner(const Game& game, int winner)
+{
     std::string winner_name;
-
     if (winner == 1)
     {
         winner = 0;
@@ -40,6 +49,17 @@ int main()
     std::cout << "les " << winner_name << " ont gagné la partie! Bravo à " <<
               game.getPlayers()[winner] << " et à "<<
               game.getPlayers()[winner + 1] << "." << std::endl;
+}
+
+int main()
+{
+    int plr = get_player_n();
+
+    Game game = Game(plr);
+
+    int winner = game.loop(plr);
+
+    print_winner(game, winner);
 
     return 0;
 }
